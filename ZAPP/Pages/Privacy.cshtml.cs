53a84dfeff045b6fbd88c21b72601e517f3651e1
Sabien <sabien_02@hotmail.com>
2021-09-27 16:06:34 +0200

    Frontend fixes

--- ZAPP/Pages/Privacy.cshtml.cs
+++ ZAPP/Pages/Privacy.cshtml.cs
@@ -10,15 +10,6 @@ namespace ZAPP.Pages
 {
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-        }
     }
 }
